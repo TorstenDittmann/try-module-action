dcb098ad48c1d675888e1600d061da5740b8c7af
Torsten Dittmann <torsten.dittmann@googlemail.com>
2025-05-02 17:21:39 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -21786,7 +21786,7 @@ async function detectPackageManager(cwd$2, options = {}) {
 }
 
 //#endregion
-//#region node_modules/.pnpm/try-module.cloud@1.0.12/node_modules/try-module.cloud/main.js
+//#region node_modules/.pnpm/try-module.cloud@1.0.13/node_modules/try-module.cloud/main.js
 const exec = promisify(cp.exec);
 /**
 * @param {string} api_key
@@ -21833,7 +21833,8 @@ async function get_version() {
 	const inside_github_action = process.env.GITHUB_ACTIONS === "true";
 	if (inside_github_action) {
 		consola.info("identified github action");
-		if (process.env.GITHUB_SHA) return process.env.GITHUB_SHA.substring(0, 7);
+		const sha = process.env.GITHUB_HEAD_SHA || process.env.GITHUB_SHA;
+		if (process.env.GITHUB_SHA) return sha.substring(0, 7);
 	}
 	const inside_gitlab_pipeline = process.env.GITLAB_CI === "true";
 	if (inside_gitlab_pipeline) {
